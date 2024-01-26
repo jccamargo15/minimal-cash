9e8815659b89ecb2b200f69944b49147d96369aa
José Carlos de Camargo <jccamargo15@gmail.com>
2024-01-25 22:26:26 -0300

    feat: clerk authentication

--- app/(auth)/(routes)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="flex items-center justify-center h-full">{children}</div>
-  )
-}
-
-export default AuthLayout
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
+
 const RootPage = () => {
-  return <div className="h-full">Minimal Cash (protected)</div>
+  return (
+    <div className="h-full">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  )
 }
 
 export default RootPage
